f2d5e58d5509da0d96343c9851aafe8d635da2a4
RitaYang <ritayang0811@gmail.com>
2023-07-31 15:19:58 +0800

    store.html finished

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-import './assets/scss/all.scss';
+import "./assets/scss/all.scss";
 
 console.log("Hello world!");
+
+// 門市據點下拉選單
+$(".dropdown-btn ").click(function (e) {
+  e.preventDefault();
+  console.log("aaa");
+  $(".dropdown-menu ").toggleClass("active");
+});

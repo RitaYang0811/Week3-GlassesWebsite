6288fe19d21d1b8009069ed4c7e5224eafff53aa
RitaYang <ritayang0811@gmail.com>
2023-07-31 18:26:02 +0800

    台北店完成 下拉選單修改成功

--- main.js
+++ main.js
@@ -8,3 +8,50 @@ $(".dropdown-btn ").click(function (e) {
   console.log("aaa");
   $(".dropdown-menu ").toggleClass("active");
 });
+// 下拉選單改變門市區域文字
+// 點選li（“.down-taipei”） >.dropdown-menu判斷取消（“active”）收合選單>span（“.dropdown-btnText”）文字內容變成（“台北市”）
+$(".down-taipei").click(function (e) {
+  console.log("bbb");
+  e.preventDefault();
+  $(".dropdown-menu").toggleClass("active");
+  $(".dropdown-btnText").text("台北市");
+});
+
+$(".down-taichung").click(function (e) {
+  e.preventDefault();
+  $(".dropdown-menu").toggleClass("active");
+  $(".dropdown-btnText").text("台中市");
+});
+
+$(".down-kaoshung").click(function (e) {
+  e.preventDefault();
+  $(".dropdown-menu").toggleClass("active");
+  $(".dropdown-btnText").text("高雄市");
+});
+
+// 下拉選單改變分店名字
+$(".down-taipei-chunshan").click(function (e) {
+  e.preventDefault();
+  $(".dropdown-menu").toggleClass("active");
+  $(".dropdown-btnText").text("台北中山旗艦店");
+});
+$(".down-taichung-greengraden").click(function (e) {
+  e.preventDefault();
+  $(".dropdown-menu").toggleClass("active");
+  $(".dropdown-btnText").text("台北綠園店");
+});
+$(".down-kaoshung-shei").click(function (e) {
+  e.preventDefault();
+  $(".dropdown-menu").toggleClass("active");
+  $(".dropdown-btnText").text("台中清水旗艦店");
+});
+$(".down-taichung-chunchen").click(function (e) {
+  e.preventDefault();
+  $(".dropdown-menu").toggleClass("active");
+  $(".dropdown-btnText").text("高雄中正形象店");
+});
+$(".down-kaoshung-dream").click(function (e) {
+  e.preventDefault();
+  $(".dropdown-menu").toggleClass("active");
+  $(".dropdown-btnText").text("高雄夢時代店<");
+});
